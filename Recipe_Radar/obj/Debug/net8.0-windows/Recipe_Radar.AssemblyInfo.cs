4feeb4c9179b641e7240b12b72706fc1bee67fd9
Oliver Cieplinski <olivercieplinski@gmail.com>
2024-01-02 16:48:57 +0000

    Added more styling to elements

--- Recipe_Radar/obj/Debug/net8.0-windows/Recipe_Radar.AssemblyInfo.cs
+++ Recipe_Radar/obj/Debug/net8.0-windows/Recipe_Radar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipe_Radar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d6b900f861f3a98acbed4af90d288f66c6559f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410a7b2968eec4e8000b07b4ccb6f05daccf1d62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipe_Radar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipe_Radar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
