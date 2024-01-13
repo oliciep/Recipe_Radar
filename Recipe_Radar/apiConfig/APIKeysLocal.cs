33c1b41ca627adb8b71c8068e87f96d259595ab0
Oliver Cieplinski <olivercieplinski@gmail.com>
2024-01-13 11:33:42 +0000

    Added SQL folder structure, refactored files

---
+++ Recipe_Radar/apiConfig/APIKeysLocal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Radar.config
+{
+    public static partial class APIKeys
+    {
+        static APIKeys()
+        {
+            SpoonacularKey = "288d4b3d9f8d44d39f041ea5260c4301";
+        }
+    }
+}
